cdefb55d6b0967739e905cfacb207d1930668dd8
ragib_sh <ragibsh.me@gmail.com>
2022-04-08 22:07:29 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# django_log_tracker: A Django app to conduct api logs
+![version](https://img.shields.io/badge/version-1.0.1-blue.svg)
+[![Open Source](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://opensource.org/)
+
+<a href="https://github.com/Ragib01/django_log_tracker"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" alt="GitHub"/></a>
+
+It logs all the API information for content type "application/json".
+
+## Database fields:
+
+- status
+- api
+- method
+- headers
+- client_ip_address
+- client_ip_geolocation
+- response
+- status_code
+- execution_time
+- user_agent
+- added_on
+
+## Installation
+
+1. PyPI
+
+```sh
+pip install django-log-tracker
+```
+
+2. Add "django_log_tracker" to your INSTALLED_APPS setting like this
+
+```python
+INSTALLED_APPS = [
+    ...,
+    'django_log_tracker',
+]
+```
+
+3.  Add in MIDDLEWARE
+
+```python
+MIDDLEWARE = [
+    ...,
+    'django_log_tracker.middleware.log_tracker_middleware.LogTrackerMiddleware',
+]
+```
+
+4. Run
+
+```shell
+python manage.py migrate
+```
+
+####  Add these lines in settings file.
+
+
+### Store logs into the database
+
+Log every request into the database.
+
+```python
+DJANGO_LOG_TRACKER_DATABASE = True  # Default to False
+```
+
+- Logs will be available in Django Admin Panel.
+- The search bar will search in Request Body, Response, Headers and API URL.
+- You can also filter the logs based on the "added_on" date, Status Code and Request Methods.
+
+### Track client ip address
+
+```python
+DJANGO_LOG_TRACKER_IP = True # Default to False
+```
+
+### Track client ip geolocation
+
+```python
+DJANGO_LOG_TRACKER_IP_GEOLOCATION = True # Default to False
+```
+
+#### Note:
+> API response may get slower for ***DJANGO_LOG_TRACKER_IP*** and
+***DJANGO_LOG_TRACKER_IP_GEOLOCATION***. So by default they both are false.
---
+++ setup.py
@@ -0,0 +1,59 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="django_log_tracker",
+    version="1.0.1",
+    description="A Django app to conduct api logs",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ragib01/django_log_tracker.git",
+    author="Ragib Shahriar",
+    author_email="ragibsh.me@gmail.com",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    project_urls={
+        'Source': 'https://github.com/Ragib01/django_log_tracker'
+    },
+    packages=[
+        'django_log_tracker',
+        'django_log_tracker.middleware',
+        'django_log_tracker.migrations'
+    ],
+    include_package_data=True,
+    install_requires=[],
+)
+
+# (*) Please direct queries to the discussion group, rather than to me directly
+#     Doing so helps ensure your question is helpful to other users.
+#     Queries directly to my email are likely to receive a canned response.
+#
+#     Many thanks for your understanding.
